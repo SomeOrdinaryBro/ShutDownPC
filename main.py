d1df5dee1c42e45a1641d7abdad5ce6b79d96f65
Sajid <sajid@duck.com>
2023-07-01 12:16:39 +0530

    Update main.py
    activity detection added / also restart

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import ctypes
 import keyboard
 import logging
 
-INACTIVITY_THRESHOLD = 540
-COUNTDOWN_THRESHOLD = 60
+INACTIVITY_THRESHOLD = 600  
+COUNTDOWN_DURATION = 60  
 MAX_SHUTDOWN_RETRIES = 3
+
+countdown_active = False
 
 def display_shutdown_warning(message):
     ctypes.windll.user32.MessageBoxW(0, message, "PC Shutdown Warning", 0x40 | 0x1)
@@ -23,6 +25,8 @@ def check_activity():
 
     keyboard.on_press(on_keyboard_activity)
 
+    global countdown_active
+
     while True:
         current_mouse_position = pyautogui.position()
         current_time = time.time()
@@ -31,16 +35,30 @@ def check_activity():
             last_mouse_position = current_mouse_position
             last_activity_time = current_time
 
+            if countdown_active:
+                countdown_active = False
+
         mouse_inactivity_time = current_time - last_activity_time
 
-        if mouse_inactivity_time >= INACTIVITY_THRESHOLD:
+        if mouse_inactivity_time >= INACTIVITY_THRESHOLD and not countdown_active:
+            countdown_active = True
             display_shutdown_warning("PC will shut down in 1 minute.")
-            threading.Timer(COUNTDOWN_THRESHOLD, initiate_shutdown).start()
+            threading.Timer(COUNTDOWN_DURATION, initiate_shutdown).start()
             break
 
 def initiate_shutdown(retries=0):
     try:
-        subprocess.call(['shutdown', '/s', '/f', '/t', '0'], shell=True)
+        global countdown_active
+
+        if countdown_active:
+            for remaining_time in range(COUNTDOWN_DURATION, 0, -1):
+                if not countdown_active:
+                    return
+
+                display_shutdown_warning(f"PC will shut down in {remaining_time} seconds.")
+                time.sleep(1)
+
+            subprocess.call(['shutdown', '/s', '/f', '/t', '0'], shell=True)
     except subprocess.CalledProcessError as e:
         logging.error("An error occurred while trying to shut down the PC: %s", str(e))
 
